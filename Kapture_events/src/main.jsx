19c9d5467db27a5345235ebdb73c1209b5b1f40f
animesh-2002 <93852453+animesh-2002@users.noreply.github.com>
2024-03-12 20:50:59 +0530

    Admin Login Page

--- Kapture_events/src/main.jsx
+++ Kapture_events/src/main.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import Home_page from './page/home_page/Home_page.jsx'
 import CreateEvent from "./page/event_page/CreateEvent.jsx";
 import Registration_page from "./page/registration_page/Registration_page.jsx";
+import Login_page from "./page/admin_page/Login_page.jsx";
+import Home from "./components/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-  <CreateEvent/>
-    
+  <Login_page/>
   </React.StrictMode>,
 )
