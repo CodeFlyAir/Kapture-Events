b3d2799abaf60fa83ebcce288a363259bbf19262
animesh-2002 <93852453+animesh-2002@users.noreply.github.com>
2024-03-14 00:41:29 +0530

    Added Admin Approval

--- Kapture_events/src/main.jsx
+++ Kapture_events/src/main.jsx
@@ -7,9 +7,12 @@ import CreateEvent from "./page/event_page/CreateEvent.jsx";
 import Registration_page from "./page/registration_page/Registration_page.jsx";
 import Login_page from "./page/admin_page/Login_page.jsx";
 import Home from "./components/Home.jsx";
+import AdminApproval from "./components/AdminApproval.jsx";
+import Admin_Approval from "./page/admin_approval/Admin_Approval.jsx";
+import Society_Login from "./page/societylogin_page/Society_Login.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Login_page/>
+  <Home_page/>
   </React.StrictMode>,
 )
---
+++ Kapture_events/src/page/societylogin_page/Society_Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../../components/header.jsx';
+import Login_Admin from "../../components/Login_Admin.jsx";
+import AdminApproval from "../../components/AdminApproval.jsx";
+import SocietyLogin from "../../components/SocietyLogin.jsx";
+export default function Society_Login() {
+    return (
+        <SocietyLogin/>
+
+    );
+}
